e973369ceb2da075d148e73d79738454d3162115
sander <sanderdsz@gmail.com>
2023-03-08 16:22:55 -0300

    chore: name refactor

--- src/repositories/getUserDataExternaByUserId.repository.ts
+++ src/repositories/getUserDataExternaByUserId.repository.ts
@@ -1,6 +1,6 @@
 import { query } from "../utils/databases/mysql";
 
-export const getUserDataExternalByUserId = async (user_id: number) => {
+export const getUserDataExternalByUserIdRepository = async (user_id: number) => {
 	const queryString = `SELECT * FROM user_data_external WHERE user_id = ${user_id}`;
 	const data = await query(queryString);
 	return data[0];
