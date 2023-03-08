2a09888a66c7eae381532d63c80e382eee5ff5eb
sander <sanderdsz@gmail.com>
2023-03-08 16:21:54 -0300

    chore: env changes

--- src/services/github/getGithubAuthorization.service.ts
+++ src/services/github/getGithubAuthorization.service.ts
@@ -2,12 +2,12 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const GITHUB_OAUTH_CLIENT_ID = process.env
-	.GITHUB_OAUTH_CLIENT_ID as unknown as string;
+const GITHUB_APP_OAUTH_URL = process.env
+	.GITHUB_APP_OAUTH_URL as unknown as string;
 
 /*
  * Function that construct the Auth entrypoint for GitHub.
  */
 export const getGithubAuthorization = () => {
-	return `${"https://github.com/apps/node-auth"}`;
+	return `${GITHUB_APP_OAUTH_URL}`;
 };
